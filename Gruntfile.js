2ff690a99f48f457fa75bbc44aaa8d4d28510c6b
DonPage <don.page.work@gmail.com>
2014-12-31 10:37:28 -0600

    min most of the js/css files

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+/**
+ * Created by donpage on 12/31/14.
+ */
+
+module.exports = function(grunt){
+    grunt.initConfig({
+        min:{
+            dist:{
+                src:'js/service.js',
+                dest:'js/service.min.js'
+            }
+        }
+    })
+};
