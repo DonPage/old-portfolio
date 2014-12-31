97a12a04e06ff4554adb9f95b8fbba14493173d5
DonPage <don.page.work@gmail.com>
2014-12-31 15:17:12 -0600

    grunt file now working with ngAnn and uglify, ran grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,61 @@
-/**
- * Created by donpage on 12/31/14.
- */
+module.exports = function (grunt) {
 
-module.exports = function(grunt){
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.loadNpmTasks('grunt-ng-annotate');
+
     grunt.initConfig({
-        min:{
-            dist:{
-                src:'js/service.js',
-                dest:'js/service.min.js'
+
+        ngAnnotate: {
+            options: {
+                singleQuotes: true
+            },
+            app1: {
+                files: [
+                    {
+                        expand: true,
+                        src: ['js/main.js'],
+                        ext: '.annotated.js', // Dest filepaths will have this extension.
+                        extDot: 'last'       // Extensions in filenames begin after the last dot
+                    }
+                ]
+            }
+        },
+
+
+        uglify: {
+            serv: {
+                options: {
+                    report: 'min',
+                    mangle: true
+                },
+                files: {
+                    'js/appService.min.js': ['js/service.js']
+                }
+            },
+
+            main: {
+                options: {
+                    report: 'min',
+                    mangle: false
+                },
+
+                files: {
+                    'js/appMain.min.js': ['js/main.annotated.js']
+                }
+
             }
         }
-    })
+
+
+    });
+
+
+    grunt.registerTask('ugly', ['uglify']);
+
+    grunt.registerTask('ngAnn', ['ngAnnotate']);
+
+
 };
+
+
